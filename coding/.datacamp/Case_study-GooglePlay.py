7283f99d6890fd732ab2f1c335642d6972b3e17d
springrolls1703 <ryan.nguyen1008@gmail.com>
2020-05-31 15:01:37 +0700

    Google PLay Case Study

--- coding/.datacamp/Case_study-GooglePlay.py
+++ coding/.datacamp/Case_study-GooglePlay.py
@@ -1,3 +1,11 @@
+#Key learning point
+#Removing characters in number columns for cleaning purpose: using for loop
+#Plotting using Plotly: layout, data 
+#Filter using isin
+#The usage of Jointplot, Boxplot
+#Sentiment analysis
+
+
 # Read in dataset
 import pandas as pd
 apps_with_duplicates = pd.read_csv('datasets/apps.csv')
@@ -68,3 +76,86 @@ layout = {'shapes': [{
           }
 
 plotly.offline.iplot({'data': data, 'layout': layout})
+
+%matplotlib inline
+import seaborn as sns
+sns.set_style("darkgrid")
+import warnings
+warnings.filterwarnings("ignore")
+
+# Filter rows where both Rating and Size values are not null
+apps_with_size_and_rating_present = apps[(~apps['Rating'].isnull()) & (~apps['Size'].isnull())]
+
+# Subset for categories with at least 250 apps
+large_categories = apps_with_size_and_rating_present.groupby('Category').filter(lambda x: len(x) >= 250).reset_index()
+
+# Plot size vs. rating
+plt1 = sns.jointplot(x = large_categories['Size'], y = large_categories['Rating'], kind = 'hex')
+
+# Subset apps whose 'Type' is 'Paid'
+paid_apps = apps_with_size_and_rating_present[apps_with_size_and_rating_present['Type'] == 'Paid']
+
+# Plot price vs. rating
+plt2 = sns.jointplot(x = paid_apps['Price'], y = paid_apps['Rating'])
+
+
+----
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+fig.set_size_inches(15, 8)
+
+# Select a few popular app categories
+popular_app_cats = apps[apps.Category.isin(['GAME', 'FAMILY', 'PHOTOGRAPHY',
+                                            'MEDICAL', 'TOOLS', 'FINANCE',
+                                            'LIFESTYLE','BUSINESS'])]
+
+# Examine the price trend by plotting Price vs Category
+ax = sns.stripplot(x = popular_app_cats['Price'], y = popular_app_cats['Category'], jitter=True, linewidth=1)
+ax.set_title('App pricing trend across categories')
+
+# Apps whose Price is greater than 200
+apps_above_200 = popular_app_cats[['Category', 'App', 'Price']][popular_app_cats['Price'] > 200]
+apps_above_200
+---
+
+trace0 = go.Box(
+    # Data for paid apps
+    y=apps[apps['Type'] == 'Paid']['Installs'],
+    name = 'Paid'
+)
+
+trace1 = go.Box(
+    # Data for free apps
+    y=apps[apps['Type'] == 'Free']['Installs'],
+    name = 'Free'
+)
+
+layout = go.Layout(
+    title = "Number of downloads of paid apps vs. free apps",
+    yaxis = dict(
+        type = 'log',
+        autorange = True
+    )
+)
+
+# Add trace0 and trace1 to a list for plotting
+data = [trace0, trace1]
+plotly.offline.iplot({'data': data, 'layout': layout})\
+
+--
+# Load user_reviews.csv
+reviews_df = pd.read_csv('datasets/user_reviews.csv')
+
+# Join and merge the two dataframe
+merged_df = pd.merge(apps, reviews_df, on = 'App', how = "inner")
+
+# Drop NA values from Sentiment and Translated_Review columns
+merged_df = merged_df.dropna(subset=['Sentiment', 'Translated_Review'])
+
+sns.set_style('ticks')
+fig, ax = plt.subplots()
+fig.set_size_inches(11, 8)
+
+# User review sentiment polarity for paid vs. free apps
+ax = sns.boxplot(x = 'Type', y = 'Sentiment_Polarity', data = merged_df)
+ax.set_title('Sentiment Polarity Distribution')
