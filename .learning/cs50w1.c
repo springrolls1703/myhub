5776144fc4885536bc75e4dc97c4e3ed79b3157d
ryannguyen1703 <ryan.nguyen1008@gmail.com>
2020-09-28 20:15:17 +0700

    cs50 week1

--- .learning/cs50w1.c
+++ .learning/cs50w1.c
@@ -1,6 +1,25 @@
-#include <stdio.h>
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     printf("hello, world\n");
+// }
+
+//$ clang cs50wq.c
+//$ a.out
+//$ clang -o test cs50.c
+//$ 
+
+include <cs50.h>
+include <stdio.h>
 
 int main(void)
 {
-    printf("hello, world\n");
+    string answer = get_string("What's your name?\n");
+    printf("hello, %s\n", answer);
 }
+
+// place holder: %s string, %c char, %f float, double, %i int, %li long
+
+// you can use make {{file.name}}
+
