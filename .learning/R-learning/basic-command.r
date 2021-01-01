29f4a4e55387cc68c03ba8ba6e6ccb29650fcaab
Thang Nguyen <thangnguyen@Thangs-MacBook-Pro.local>
2021-01-01 19:05:09 +0700

    statistics and R

---
+++ .learning/R-learning/basic-command.r
@@ -0,0 +1,11 @@
+getwd() - get current directory
+ls()
+list.files()/ dir() - List all the files in your working directory using list.files() or dir().
+args() - Using the args() function on a function name is also a handy way to see what arguments a
+| function can take.
+dir.create("testdir") - create new directory called testdir
+setwd("testdir") - set current directory to testdir
+file.create(mytest.R)
+
+--Number
+seq(1,20)/ 1:20
